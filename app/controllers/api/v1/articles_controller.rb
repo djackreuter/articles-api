66151f366a42cbc9657c127117ad3d2d596add55
Jack Reuter <djreuter45@gmail.com>
2017-12-01 20:47:21 -0700

    fixed syntax error in articles controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ArticlesController < ApplicationController
       def index
-        @articles = Article.order('created_at DESC')
+        articles = Article.order('created_at DESC')
         render json: {status: 200, message: 'Request successful', data:articles}, status: :ok
       end
     end
