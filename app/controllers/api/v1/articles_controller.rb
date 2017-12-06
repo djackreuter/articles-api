e0095b7a0706e5dac965017e24edd030f38007d5
Jack Reuter <djreuter45@gmail.com>
2017-12-05 17:34:21 -0700

    added create method

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -10,7 +10,22 @@ module Api
         article = Article.find(params[:id])
         render json: {status: 200, message: 'Request successful', data:article}, status: :ok
       end
-      
+
+      def create
+        article = Article.new(article_params)
+
+        if article.save()
+          render json: {status: 200, message: 'Article saved', data:article}, status: :ok
+        else
+          render json: {status: 418, message: 'Something went wrong', data:article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+        def article_params
+          params.permit(:title, :body)
+        end
+
     end
   end
 end
